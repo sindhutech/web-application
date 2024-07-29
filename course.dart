d964dc5a548a22aa0b4786de87918f9f5b0a642a
A R SINDHU <162720777+sindhutech@users.noreply.github.com>
2024-07-29 23:27:05 +0530

    Create course.dart

---
+++ course.dart
@@ -0,0 +1,15 @@
+class Course {
+  final String id;
+  final String name;
+  final String instructor;
+  final String description;
+  
+
+  Course({
+    required this.id,
+    required this.name,
+    required this.instructor,
+    required this.description,
+    
+  });
+}
